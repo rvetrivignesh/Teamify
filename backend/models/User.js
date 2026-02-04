747dc2029f8e41c180fff6eba203bccc2d5ebc25
R Vetri Vignesh <rvetrivignesh@Rs-MacBook-Air.local>
2026-02-04 07:19:24 +0530

    backend | add databasae model for user

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", function () {
+    if (!this.isModified("password")) {
+        return;
+    }
+    const salt = bcrypt.genSaltSync(10);
+    this.password = bcrypt.hashSync(this.password, salt);
+});
+
+userSchema.methods.matchPassword = function (enteredPassword) {
+    return bcrypt.compareSync(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;    
