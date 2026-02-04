a916aec08b16e0d6e8890678bfb1fc901eaf160d
R Vetri Vignesh <rvetrivignesh@Rs-MacBook-Air.local>
2026-02-04 23:20:34 +0530

    backend | setup backend routes and cors to listen to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/AuthRoutes.js";
+import profileRoutes from "./routes/ProfileRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend server!');
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend server!");
 });
 
 app.listen(PORT, () => {
