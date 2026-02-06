d406497ccb952ba18c02765014ad04ca94d5cb4c
rvetrivignesh <rvetrivignesh@.com>
2026-02-06 21:09:59 +0530

    frontend | update style sheet route

--- frontend/src/pages/Profile/UserProfile.jsx
+++ frontend/src/pages/Profile/UserProfile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../services/api";
-import "./UserProfile.css";
+import "./Profile.css";
 
 const UserProfile = () => {
   const { username } = useParams();
