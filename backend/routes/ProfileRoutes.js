672d813c4b77e1a36a66af3dc635634fa58974c4
R Vetri Vignesh <rvetrivignesh@Rs-MacBook-Air.local>
2026-02-06 16:59:35 +0530

    backend | add profile route for fetching user by name

--- backend/routes/ProfileRoutes.js
+++ backend/routes/ProfileRoutes.js
@@ -1,26 +1,9 @@
 import express from 'express';
 import UserProfile from '../models/UserProfile.js';
+import User from '../models/User.js';
 import protect from '../middleware/Authenticate.js';
 
 const router = express.Router();
-
-router.get('/me', protect, async (req, res) => {
-    try {
-        const profile = await UserProfile.findOne({ user: req.user._id }).populate(
-            'user',
-            'username email',
-        );
-
-        if (!profile) {
-            return res.status(404).json({ message: 'Profile not found' });
-        }
-
-        res.json(profile);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
 
 router.post("/", protect, async (req, res) => {
     try {
@@ -56,5 +39,28 @@ router.post("/", protect, async (req, res) => {
     }
 });
 
+// Get user profile by Username
+router.get("/u/:username", protect, async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.params.username });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const profile = await UserProfile.findOne({ user: user._id }).populate(
+            "user",
+            "username email"
+        );
+
+        if (!profile) {
+            return res.status(404).json({ message: "Profile not found" });
+        }
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
 
 export default router;
