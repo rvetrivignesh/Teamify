4eaa1455545c776f418902b88faefa5611c2ba71
rvetrivignesh <rvetrivignesh@.com>
2026-02-06 21:55:09 +0530

    frontend | prepare server for deployment

--- backend/server.js
+++ backend/server.js
@@ -14,19 +14,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://rvetrivignesh.github.io",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://rvetrivignesh.github.io",
+    "https://rvetrivignesh.github.io/"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
