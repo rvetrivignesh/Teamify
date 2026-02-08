94217aeac7548bcf3fb2db694dcd6f65da5a0190
rvetrivignesh <rvetrivignesh@.com>
2026-02-08 09:59:37 +0530

    backend | modify project model to include domain and repo-link

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -23,6 +23,16 @@ const projectSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        domain: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
+        repositoryLink: {
+            type: String,
+            trim: true,
+        },
         skillsRequired: {
             type: [String],
             default: [],
